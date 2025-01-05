5d676d8eb1fc82aa730c5e93a700d809a3571f87
PopescuCristian1 <popescucristian2004@gmail.com>
2025-01-06 01:51:25 +0200

    Update Team.cpp

--- src/Team.cpp
+++ src/Team.cpp
@@ -1,28 +1,28 @@
 #include "Team.h"
 
-// Inițializează singleton-ul
+
 std::unique_ptr<Team> Team::instance = nullptr;
 
 Team* Team::getInstance() {
     if (!instance) {
-        instance = std::unique_ptr<Team>(new Team()); // Alocare pe heap
+        instance = std::unique_ptr<Team>(new Team()); 
     }
     return instance.get();
 }
 
 void Team::addUtility(const Utility& utility) {
-    utilities.push_back(utility);
+    utilities.push_back(utility); 
 }
 
 void Team::displayUtilities() const {
     for (const auto& utility : utilities) {
-        std::cout << utility << '\n';
+        std::cout << utility << '\n'; 
     }
 }
 
 void Team::sortUtilities() {
     if (utilities.empty()) {
-        throw CustomException();
+        throw CustomException(); 
     }
     std::sort(utilities.begin(), utilities.end(), [](const Utility& a, const Utility& b) {
         return a < b;
